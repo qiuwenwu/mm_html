152c6f674ee23922f06fdbee92364e7e05631644
邱文武 <573242395@qq.com>
2021-03-15 10:38:46 +0800

    版本1.1.0

--- test.js
+++ test.js
@@ -1,80 +1,28 @@
-// const nJwt = require('njwt');
-var Http = require('mm_https');
-var Html = require('./index.js');
-var fs = require("fs");
-
-function file(html_str){
-	return html_str.replace(/<[^<>]+>/g,'')
-}
-
-function model(o) {
-	return {
-		code: o.eq(0).text().trim(),
-		name: o.eq(2).text().trim(),
-		time_to_market: o.eq(3).text(),
-		num: Number(o.eq(5).text()),
-		per: Number(o.eq(7).text()),
-		issue_price: Number(o.eq(8).text())
-	}
-}
+var Dom = require('./index.js');
 
 /**
- * 获取股票信息
- * @param {Number} page 页码
+ * 测试html转为json
+ * @param {String} file html文件
  */
-async function get_stock (page) {
-	var hp = new Http();
-	var url = `http://vip.stock.finance.sina.com.cn/corp/view/vRPD_NewStockIssue.php?page=${page}&cngem=0&orderBy=NetDate&orderType=desc`;
-	var res = await hp.get(url);
+async function test(file) {
+	var dom = new Dom();
+	var text = file.loadText(__dirname + '/');
+	// var text = `<body><script src="assets/js/owl.carousel.js"></script></body>`;
 	
-	// 使用jquery读取节点内容
-	var html = new Html();
-	var jq = html.toJQ(res.body);
-	// 获取到单行数据
-	var trs = jq('#NewStockTable tbody tr');
-	var len = trs.length;
-	var list = [];
-	for(var i = 2; i < len; i++){
-		var o = trs.eq(i);
-		var tds = o.children('td');
-		if(tds.length > 9){
-			var m = model(tds);
-			list.push(m);
-		}
-	}
-	return list;
+// 	var text = `
+// <div class="breadcrum-bg py-sm-5 py-4">bbbbb
+// 				<div class="container py-lg-3">
+// 					<h2>About Us</h2>
+// 					<p><a href="index.html">Home</a> &nbsp; / &nbsp; About</p>
+// 				</div>
+// 				<div v-model="form.think"><p>ccc</p>123123</div>
+// 			</div>`;
+	var json = dom.toJson(text);
+	'./demo_dict.json'.saveJson(json, true);
+	
+	var str = dom.toHtml(json);
+	'./demo_dict.html'.saveText(str);
+	console.log(json);
 }
 
-/**
- * 获取页数
- */
-async function get_pages () {
-	var hp = new Http();
-	var url = `http://vip.stock.finance.sina.com.cn/corp/view/vRPD_NewStockIssue.php?page=1&cngem=0&orderBy=NetDate&orderType=desc`;
-	var res = await hp.get(url);
-	
-	var mh = res.body.match(/共[0-9]+页/);
-	if(mh){
-		var num = mh[0].replace('共', '').replace('页', '');
-		return Number(num);
-	}
-	else {
-		return 0;
-	}
-}
-
-async function test (page) {
-	var len = await get_pages();
-	console.log(`共${len}页`);
-	var list = [];
-	for(var i = 0; i < len; i++){
-		var lt = await get_stock(i + 1);
-		list.addList(lt);
-		// 休眠1秒
-		$.sleep(1);
-		break;
-	}
-	console.log(list);
-	console.log(list.length);
-}
-test();
+test('./demo.html');
